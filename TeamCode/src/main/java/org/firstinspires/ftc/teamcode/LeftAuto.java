66185aa27b66255fa0bafce9971700cb716a38f2
LuluC7777 <luyang.chen78@K12.leanderisd.org>
2023-01-04 17:37:06 -0600

    added camera and sleeve functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuto.java
@@ -10,6 +10,19 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
 @Autonomous(name="Left Auto")
 
 public class LeftAuto extends LinearOpMode {
@@ -17,7 +30,27 @@ public class LeftAuto extends LinearOpMode {
     public CRServo extension;
     public Servo rightClaw, leftClaw;
 
-    private double right = 300, left = -300;
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+    // Tag Ids for sleeve
+    int left = 3;
+    int middle = 4;
+    int right = 6;
+
+    AprilTagDetection tagOfInterest = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -44,9 +77,13 @@ public class LeftAuto extends LinearOpMode {
         slide.setPower(1);
 
         parentAuton bot = new parentAuton();
+
+        int sleeveID = getSleeveID();
+
+
         waitForStart();
         while (opModeIsActive()) {
-            bot.driveForward(5.0, 1900, frontRight, frontLeft, backRight, backLeft);
+            /*bot.driveForward(5.0, 1900, frontRight, frontLeft, backRight, backLeft);
             bot.rotateLeft(5.0, 450, frontRight, frontLeft, backRight, backLeft);
             bot.driveForward(5.0, 530, frontRight, frontLeft, backRight, backLeft);
             bot.closeClaw(leftClaw, rightClaw);
@@ -72,7 +109,102 @@ public class LeftAuto extends LinearOpMode {
             //bot.openClaw(leftClaw, rightClaw);
             //bot.closeClaw(leftClaw, rightClaw);
             //bot.raiseExtension(-2000, extension);
+            */
+            if (sleeveID == left) {
+                bot.driveBackward(5, 850, frontRight, frontLeft, backRight, backLeft);
+                sleep(200);
+                bot.rotateRight(5, 100, frontRight, frontLeft, backRight, backLeft);
+                bot.strafeRight(5, 1000, frontRight, frontLeft, backRight, backLeft);
+            } else if (sleeveID == right) {
+                bot.driveBackward(5, 900, frontRight, frontLeft, backRight, backLeft);
+                sleep(200);
+                bot.rotateRight(5, 100, frontRight, frontLeft, backRight, backLeft);
+                bot.strafeLeft(5, 900, frontRight, frontLeft, backRight, backLeft);
+            } else {
+                bot.driveBackward(5, 900, frontRight, frontLeft, backRight, backLeft);
+            }
             stop();
         }
+    }
+
+    public int getSleeveID() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(640, 360, OpenCvCameraRotation.SIDEWAYS_RIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
+        while (!isStarted() && !isStopRequested()) {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+            if (currentDetections.size() != 0) {
+                boolean tagFound = false;
+                for (AprilTagDetection tag : currentDetections) {
+                    if (tag.id == left || tag.id == middle || tag.id == right) {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if (tagFound) {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                } else {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if (tagOfInterest == null) {
+                        telemetry.addLine("(The tag has never been seen)");
+                    } else {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+            } else {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if (tagOfInterest == null) {
+                    telemetry.addLine("(The tag has never been seen)");
+                } else {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+            }
+            telemetry.update();
+            sleep(20);
+        }
+
+        /* Update the telemetry */
+        if (tagOfInterest != null) {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        } else {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :( L ");
+            telemetry.update();
+        }
+
+        return tagOfInterest.id;
+    }
+
+    void tagToTelemetry(AprilTagDetection detection) {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
     }
 }
