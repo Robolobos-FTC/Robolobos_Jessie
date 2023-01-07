23d1f4d59fafd971142f9105d8b9b33b56805d45
LuluC7777 <luyang.chen78@K12.leanderisd.org>
2023-01-07 15:15:24 -0600

    broken extension code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuto.java
@@ -71,10 +71,10 @@ public class LeftAuto extends LinearOpMode {
 
         // Slide
         slide.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        //slide.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
-        slide.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         slide.setTargetPosition(0);
-        slide.setPower(1);
+        slide.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+        //slide.setPower(1);
 
         parentAuton bot = new parentAuton();
 
@@ -83,39 +83,14 @@ public class LeftAuto extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
-            bot.driveForward(5.0, 1900, frontRight, frontLeft, backRight, backLeft);
-            bot.rotateLeft(5.0, 450, frontRight, frontLeft, backRight, backLeft);
-            bot.driveForward(5.0, 530, frontRight, frontLeft, backRight, backLeft);
             bot.closeClaw(leftClaw, rightClaw);
-            bot.raiseSlide(1000, slide);
-            bot.driveBackward(5.0, 500, frontRight, frontLeft, backRight, backLeft);
-            bot.rotateLeft(5.0, 1100, frontRight, frontLeft, backRight, backLeft);
-            bot.driveForward(5.0, 590, frontRight, frontLeft, backRight, backLeft);
-            bot.raiseSlide(4199, slide);
-            bot.driveForward(5.0, 150, frontRight, frontLeft, backRight, backLeft);
-            bot.openClaw(leftClaw, rightClaw);
-            bot.driveBackward(5.0, 500, frontRight, frontLeft, backRight, backLeft);
-            bot.raiseSlide(0, slide);
-            bot.rotateLeft(5.0, 850, frontRight, frontLeft, backRight, backLeft);
-            bot.driveForward(5.0, 700, frontRight, frontLeft, backRight, backLeft);
-            bot.closeClaw(leftClaw, rightClaw);
-            //bot.strafeRight(3.0, 2000, frontRight, frontLeft, backRight, backLeft);
-            //bot.strafeLeft(3.0, 2000, frontRight, frontLeft, backRight, backLeft);
-            //bot.rotateRight(3.0, 2000, frontRight, frontLeft, backRight, backLeft);
-            //bot.rotateLeft(3.0, 3000, frontRight, frontLeft, backRight, backLeft);
-            //bot.raiseSlide(4000, 1000, slide);
-            //bot.raiseExtension(3000, extension);
-            //bot.raiseSlide(0, 0, slide);
-            //bot.openClaw(leftClaw, rightClaw);
+            bot.raiseExtension(300, extension);
+            //extension.setPower(1);
+            sleep(10000);
+            //bot.strafeRight(5, 2200, frontRight, frontLeft, backRight, backLeft);
+            //bot.strafeLeft(5,400, frontRight, frontLeft, backRight, backLeft);
+            //bot.driveForward(5,800, frontRight, frontLeft, backRight, backLeft);
             //bot.closeClaw(leftClaw, rightClaw);
-            //bot.raiseExtension(-2000, extension);
-
-
-
-
-            // DKFLJDSLKFJlsdf
-            ///fdskfljsdlkfjdslf
-
 
 
             // Below code to confirm camera can detect sleeve
